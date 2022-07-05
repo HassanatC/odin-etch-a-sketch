e6120d551870fb1317de13ad1daed293b5297706
HassanatC <hassanat431@hotmail.co.uk>
2022-07-05 21:08:09 +0100

    UI Changes

--- etchasketch.js
+++ etchasketch.js
@@ -2,41 +2,44 @@
 function createBoard(size) {
 
 
-let board = document.querySelector('.board');
-let squares = board.querySelectorAll('div');
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
 
-squares.forEach(div => div.remove());
+    squares.forEach(div => div.remove());
 
 
-board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
-board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
 
 
 // loop is used to generate the board and the square divs on the sketchpad.
 
-let amount = size * size;
+    let amount = size * size;
 
 
-    for(let i = 0; i < amount; i++) {
+        for(let i = 0; i < amount; i++) {
 
-    let square = document.createElement('div');
-    square.style.backgroundColor = 'white';
-    board.insertAdjacentElement('beforeend', square);
-    
-    // Mouse over and event listener causes the white square to turn black after putting cursor over it.
+        let square = document.createElement('div');
+        square.style.backgroundColor = 'white';
+        board.insertAdjacentElement('beforeend', square);
+        
+        // Mouse over and event listener causes the white square to turn black after putting cursor over it.
 
-    square.addEventListener('mouseover', function changeSquare() {
+        square.addEventListener('mouseover', function changeSquare() {
 
-        square.style.backgroundColor = 'black';
+            square.style.backgroundColor = 'black';
 
-        })
+            })
 
-    
+        
 
+        }
+
+        
     }
 
-}
+    
 
 // when called, upon using let amount = size * size, createBoard creates a board based on the input variables.
 
@@ -62,8 +65,10 @@ function changeBoard(input) {
 const resetButton = document.getElementById('reset');
 
 resetButton.addEventListener('click', function resetBoard() {
+
     board.innerHTML = " ";
     createBoard(16, 16);
+
 });
 
 
